5f9c6bc5b9270f755855c821848e35a5dd4a869f
Lakmal Warusawithana <lakmal@wso2.com>
2015-03-02 03:46:23 +0530

    fixing url

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 <center>
-<h1>Apache Stratos PHP Demo</h1>
-<H2>www.stratos.apache.org</h2>
+<h1><a href="http://www.w3schools.com">Apache Stratos PHP Demo</a></h1>
+<H2>stratos.apache.org</h2>
 </br>
 <hr style="width:50%;" style="height:2em;"/>
